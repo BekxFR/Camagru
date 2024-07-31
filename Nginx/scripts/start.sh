48c39dea39e0c2d1f6dced8bd25e3410afab80fe
BekxFR <104771705+BekxFR@users.noreply.github.com>
2024-07-31 18:24:51 +0200

    update docker config for use Nginx like reverse proxy and frontend for static file and php-fpm for php backend server, use only read on nginx html file and write read on php server

--- Nginx/scripts/start.sh
+++ Nginx/scripts/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+webroot=/var/www/html
+
 echo date.timezone=Europe/London > /usr/local/etc/php/conf.d/timezone.ini
 
 # find /var/www/html -type d -exec chmod 755 {} \;
 # find /var/www/html -type f -exec chmod 644 {} \;
 
-/usr/local/sbin/php-fpm --force-stderr --nodaemonize --fpm-config /usr/local/etc/php-fpm.d/www.conf
+/usr/sbin/nginx -g "daemon off; error_log /dev/stderr info;"
